4a5754f0479c0d19c4e9e572a264bd22a4837b6f
Bruno Nascimento Ferreira <brunonascimento@users.noreply.github.com>
2018-08-20 17:23:32 -0300

    exemplo

--- src/main/java/hello/HelloController.java
+++ src/main/java/hello/HelloController.java
@@ -8,6 +8,6 @@ public class HelloController {
 
 	@RequestMapping("/")
     public String index() {
-        return "Greetings from Spring Boot! Test Build";
+        return "Greetings from Spring Boot! Test Build bruno";
     }
 }
